5dface6b3e97fc19867700d6c51d8d3c77beb761
hamed <hamed.alemonie@gmail.com>
2021-07-13 22:00:04 +0430

    make first serializer and it's urls file

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers
+from blog.models import Article
+
+
+# create first serializer
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        # exclude = ['updated']
