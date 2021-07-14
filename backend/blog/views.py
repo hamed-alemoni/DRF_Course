d447d83ae251254ae60f80eaafc03b4ba2ed6c9d
hamed <hamed.alemonie@gmail.com>
2021-07-14 16:15:41 +0430

    add a new view for article detail(blog app)

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -9,3 +9,10 @@ class ArticleList(ListView):
     # article view to show info of articles
     def get_queryset(self):
         return Article.objects.filter(status=True)
+
+
+# this class base view show a special article
+class ArticleDetail(DetailView):
+    # get our special article
+    def get_object(self):
+        return get_object_or_404(Article.objects.filter(status=True), pk=self.kwargs.get('pk'))
