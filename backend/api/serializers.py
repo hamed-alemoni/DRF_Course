086caa0d72bb368a3665766a85ac0008f125e8f0
hamed <hamed.alemonie@gmail.com>
2021-07-14 16:12:20 +0430

    make new serializer for User module

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from blog.models import Article
+from django.contrib.auth.models import User
 
 
-# create first serializer
+# create a serializer for Article module
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
         # exclude = ['updated']
+
+
+# create a serializer for User module
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
