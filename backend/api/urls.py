5ca8adfcd481810f2f3ea8605c3ed45f459fd34e
hamed <hamed.alemonie@gmail.com>
2021-07-14 16:14:17 +0430

    add three urls for article detail, user detail and user list

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import ArticleList
+from .views import ArticleList, ArticleDetail, UserList, UserDetail
 
-app_name = 'aoi'
+app_name = 'api'
 
 urlpatterns = [
-    path('', ArticleList.as_view(), name='list')
+    path('', ArticleList.as_view(), name='list'),
+    path('<int:pk>', ArticleDetail.as_view(), name='detail'),
+    path('user/', UserList.as_view(), name='user-list'),
+    path('user/<int:pk>', UserDetail.as_view(), name='user-detail'),
 ]
