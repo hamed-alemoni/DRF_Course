8303a49024b93a02ca76bcdb39d955f2a4f28ba0
hamed <hamed.alemonie@gmail.com>
2021-07-14 15:26:33 +0430

    make file clean and add some comments

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-
-
-
-
-# Create your views here.
-# first view to make info serialize
-from rest_framework.generics import ListAPIView,ListCreateAPIView
-
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 from .serializers import ArticleSerializer
 from blog.models import Article
 
 
+# Create your views here.
+
+# first view to make info serialize
 class ArticleList(ListCreateAPIView):
+    # get all articles
     queryset = Article.objects.all()
+    # determine serializer class
     serializer_class = ArticleSerializer
