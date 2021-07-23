bee32784b444fda288e1243acdc63b8805e66726
hamed <hamed.alemonie@gmail.com>
2021-07-23 21:15:26 +0430

    add a url to create token

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('',include('blog.urls')),
-    path('api/',include('api.urls'))
+    path('api/',include('api.urls')),
+    path('api/token-auth/',obtain_auth_token)
 ]
