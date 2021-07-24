aedfc93a3564e407aa3b2dec6fad83f662708b11
hamed <hamed.alemonie@gmail.com>
2021-07-24 19:00:29 +0430

    create a new class to revoke tokens

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
 from .permissions import IsSuperuserOrAuthorReadOnly, IsStaffOrReadOnly, IsAuthorOrReadOnly
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -57,3 +59,14 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     # add a new permission for this view
     permission_classes = (IsSuperuserOrAuthorReadOnly,)
+
+
+# To revoke tokens
+class RevokeToken(APIView):
+    # add a new permission for this view
+    permission_classes = (IsAuthenticated,)
+
+    # delete the token
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=204)
