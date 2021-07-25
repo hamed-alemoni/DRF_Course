d682a7b2201bd583e3181ac37851e2f560a53a94
hamed <hamed.alemonie@gmail.com>
2021-07-25 20:34:45 +0430

    add new urls for rest-auth(ready package)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-from api.views import RevokeToken
+
+# from rest_framework.authtoken.views import obtain_auth_token
+# from api.views import RevokeToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('',include('blog.urls')),
-    path('api/',include('api.urls')),
-    path('api/token-auth/',obtain_auth_token),
-    path('api/revoke/',RevokeToken.as_view()),
+    path('', include('blog.urls')),
+    path('api/', include('api.urls')),
+    path('dj/rest-auth/', include('dj_rest_auth.urls')),
+    path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    # path('api/token-auth/',obtain_auth_token),
+    # path('api/revoke/',RevokeToken.as_view()),
 ]
