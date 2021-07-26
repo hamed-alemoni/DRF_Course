dac229294d822ae9ceb2c4558a70eeb84e6724db
hamed <hamed.alemonie@gmail.com>
2021-07-26 21:41:01 +0430

    fix a default url about reset password

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.views import  PasswordResetConfirmView
+
 
 # from rest_framework.authtoken.views import obtain_auth_token
 # from api.views import RevokeToken
@@ -24,8 +26,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('blog.urls')),
     path('api/', include('api.urls')),
-    path('dj/rest-auth/', include('dj_rest_auth.urls')),
-    path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('api/rest-auth/', include('dj_rest_auth.urls')),
+    path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('password/reset/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('api/token-auth/',obtain_auth_token),
     # path('api/revoke/',RevokeToken.as_view()),
 ]
