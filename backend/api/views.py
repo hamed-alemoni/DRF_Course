09d253750707c4b1dad5566be33b9ff478e352e9
hamed <hamed.alemonie@gmail.com>
2021-07-28 20:12:56 +0430

    remove unusable codes

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,8 +5,6 @@ from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
 from .permissions import IsSuperuserOrAuthorReadOnly, IsStaffOrReadOnly, IsAuthorOrReadOnly
 from django.contrib.auth.models import User
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
 
 
 # Create your views here.
@@ -61,12 +59,4 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsSuperuserOrAuthorReadOnly,)
 
 
-# To revoke tokens
-# class RevokeToken(APIView):
-#     # add a new permission for this view
-#     permission_classes = (IsAuthenticated,)
-#
-#     # delete the token
-#     def delete(self, request):
-#         request.auth.delete()
-#         return Response(status=204)
+
