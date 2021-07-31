cc185fc510e4cfbfd757fbb76e8e839a4f13dce6
hamed <hamed.alemonie@gmail.com>
2021-07-31 17:26:14 +0430

    use routers to provide path automatically

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import ArticleList, ArticleDetail, UserList, UserDetail
+from .views import UserViewSet, ArticleViewSet
+from rest_framework import routers
 
 app_name = 'api'
 
+router = routers.SimpleRouter()
+router.register(r'users', UserViewSet)
+router.register(r'', ArticleViewSet)
 urlpatterns = [
-    path('', ArticleList.as_view(), name='list'),
-    path('<int:pk>', ArticleDetail.as_view(), name='detail'),
-    path('user/', UserList.as_view(), name='user-list'),
-    path('user/<int:pk>', UserDetail.as_view(), name='user-detail'),
+    path('', include(router.urls))
 ]
