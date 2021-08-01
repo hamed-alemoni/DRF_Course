b1bead9bf189c8d72c522b4ad36cd8854eaf84de
hamed <hamed.alemonie@gmail.com>
2021-08-01 16:30:49 +0430

    add new app for filtering articles

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'django_filters',
 
 ]
 
@@ -133,7 +134,7 @@ REST_FRAMEWORK = {
     # add permissions
     'DEFAULT_PERMISSION_CLASSES': [
         'api.permissions.IsStaffOrReadOnly',
-    ], 
+    ],
     # add authentication
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
@@ -141,7 +142,10 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 SITE_ID = 1
